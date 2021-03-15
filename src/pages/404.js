014c9aad49c059c17c81404a839547a812d4ca3d
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-03-15 19:00:30 +0900

    add: Pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,18 @@
-import * as React from "react"
+import * as React from 'react'
+import { Header } from 'semantic-ui-react'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Header
+      as="h1"
+      textAlign="center"
+      content="Opps!"
+      subheader="404 Notfound"
+    />
   </Layout>
 )
 
