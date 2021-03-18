336bc6ff29a2e7b2df0d7980f01c9f02371068dd
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-03-18 15:04:23 +0900

    add: Aboutページ

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,23 @@
 import * as React from 'react'
-import { Header } from 'semantic-ui-react'
+import { StaticImage } from 'gatsby-plugin-image'
+import { Segment } from 'semantic-ui-react'
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import SEO from '../components/common/seo'
+import Title from '../components/common/title'
+import Section from '../components/common/section'
+import Layout from '../components/layout/layout'
+import aboutData from '../data/about-data'
 
-const SecondPage = () => (
+const About = () => (
   <Layout>
     <SEO title="About" />
-    <Header as="h1" textAlign="center" content="About" subheader="予定地" />
+    <Title name="About" />
+    <Segment basic textAlign="center">
+      <StaticImage src="../images/arrow2nd-icon.png" alt="icon" width={200} />
+      <h2>はじめまして、あろーです</h2>
+    </Segment>
+    <Section items={aboutData} />
   </Layout>
 )
 
-export default SecondPage
+export default About
