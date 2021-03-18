c1fdbdea151cbc9adaf2883a52934786a2645da4
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-03-18 15:01:49 +0900

    remove: data

--- src/components/category/button.js
+++ src/components/category/button.js
@@ -3,36 +3,22 @@ import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 import { Button as SUButton } from 'semantic-ui-react'
 
+import categoryData from '../../data/category-data'
 import * as Styles from './button.module.css'
 
-const categories = [
-  {
-    name: 'Game',
-    link: '/category/game'
-  },
-  {
-    name: 'Tool',
-    link: '/category/tool'
-  },
-  {
-    name: 'LINE Sticker',
-    link: '/category/sticker'
-  }
-]
-
 const Button = ({ active }) => {
-  const buttons = categories.map((e) => (
+  const buttons = categoryData.map((e) => (
     <SUButton
       key={e.name}
       circular
       active={e.name === active}
       size="small"
       as={Link}
-      to={e.link}
+      to={e.to}
       content={e.name}
     />
   ))
-  return <div className={Styles.categories}>{buttons}</div>
+  return <div className={Styles.buttons}>{buttons}</div>
 }
 
 Button.protoTypes = {
