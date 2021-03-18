47aa36e5c9999439f117aa995ed08eded60e52e8
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-03-18 15:04:14 +0900

    add: 404ページ

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,21 @@
 import * as React from 'react'
-import { Header } from 'semantic-ui-react'
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import Layout from '../components/layout/layout'
+import SEO from '../components/common/seo'
+import Title from '../components/common/title'
+import { Header, Segment } from 'semantic-ui-react'
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <Header
-      as="h1"
-      textAlign="center"
-      content="Opps!"
-      subheader="404 Notfound"
-    />
+    <SEO title="Oops!" />
+    <Title name="Oops!" />
+    <Segment basic textAlign="center">
+      <Header
+        as="h2"
+        content="ページが見つかりませんでした"
+        subheader="404 Not Found"
+      />
+    </Segment>
   </Layout>
 )
 
