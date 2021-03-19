3e0ece87d3ca216e23a0efd680279a287db22282
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-03-19 13:54:17 +0900

    add; 画像を追加

--- src/pages/other/totsuzen-no-shi.js
+++ src/pages/other/totsuzen-no-shi.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 
 import Detail from '../../components/detail/detail'
 
-const images = ['works/other/totsuzen-1.png']
+const images = ['works/other/totsuzen-1.png', 'works/other/totsuzen-2.png']
 
 const sections = [
   {
